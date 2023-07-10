128f72f0b841155aad8147aafbc93d5dbd744242
Anjali17aj <sayhellotoanjali@gmail.com>
2023-07-10 16:47:21 +0530

    Strings

---
+++ Day15/string concatenate/concat.java
@@ -0,0 +1,10 @@
+
+public class concat {
+    public static void main(String args[]){
+        String firstName = "Anjali";
+        String LastName = "Kumari";
+        String FullName = firstName +" " + LastName;
+        System.out.println("Her name is : " + FullName);
+    }
+    
+}
