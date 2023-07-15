029bcb4c92e8ae2ce36db28745a59b4400bec26a
Anjali17aj <sayhellotoanjali@gmail.com>
2023-07-15 11:39:28 +0530

    Bit Manipulation

---
+++ Day16/clear range of bits/rangeOfBBits.java
@@ -0,0 +1,16 @@
+
+
+public class rangeOfBBits {
+     
+    public static int clearBitsInRange(int n, int i, int j){
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int BitMask = a | b;
+        return n & BitMask;
+    }
+
+    public static void main(String args[]){
+        System.out.println(clearBitsInRange(10, 2, 4));  //10, 3
+   
+    }
+   }
