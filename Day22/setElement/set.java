714ebe35b274f8ca6901813553b248f626b286b1
Anjali17aj <sayhellotoanjali@gmail.com>
2023-08-01 12:18:33 +0530

    ArrayLists

---
+++ Day22/setElement/set.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class set {
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList<>();
+        //add operation
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+       
+        System.out.println(list);
+
+        //get operation
+        // int Element = list.get(3);
+        // System.out.println(Element);
+
+        //remove element operation.
+        // list.remove(2);
+        // System.out.println(list);
+
+        //set operation
+        list.set(2, 10);
+        System.out.println(list);
+        
+    }
+}
