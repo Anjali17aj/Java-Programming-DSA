6696babce2aca4f451c7f25ef6edb7240f16e1d0
Anjali17aj <sayhellotoanjali@gmail.com>
2023-08-01 12:18:50 +0530

    ArrayLists

---
+++ Day22/Size of ArrayList/size.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class size {
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(17);
+        list.add(12);
+        list.add(11);
+
+        System.out.println(list);
+        System.out.println(list.size()); //length of array list
+    }
+}
