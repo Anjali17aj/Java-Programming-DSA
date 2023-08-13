b2a95ce3b7c37314e685999a76ec6cce092c766c
Anjali17aj <sayhellotoanjali@gmail.com>
2023-08-13 12:16:15 +0530

    ArrayLists

---
+++ Day22/GetElement/get.java
@@ -0,0 +1,21 @@
+package GetElement;
+
+import java.util.ArrayList;
+
+public class get {
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList<>();
+        //add operation
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+       
+        System.out.println(list);
+
+        //get operation
+        int Element = list.get(3);
+        System.out.println(Element);
+    }
+}
