814e3a8b321618df53a9392adec16161527cdf21
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-10-25 16:07:03 +0530

    Delete Day15 directory

--- Day15/replaceString/replaces.java
+++
@@ -1,9 +0,0 @@
-package replaceString;
-
-public class replaces {
-        public static void main(String args[]) {
-        String str = "Prachi".replace("c", "");
-        System.out.println(str);
-        }
-        }
-
