11fa3b67a0d5668ca8138754f2cc611e9ac799d9
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-10-25 16:23:43 +0530

    Delete Day12 directory

--- Day12/lengthOfArray/lengths.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;  
-public class lengths{
-    public static void main(String args[]){
-        int marks[]=new int[10];    //int marks[] is array declaration. new int[10] creating an array using new keyword.
-        Scanner sc=new Scanner(System.in); //input
-        System.out.println("lenth of array is :" + marks.length);
-    }
-}
